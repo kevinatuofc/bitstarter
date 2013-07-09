3cc1fe5d1b6f55ec421f19f548548937748a6cbc
Junji Zhi <zhijjjj@gmail.com>
2013-07-08 20:11:12 -0700

    using fs.readFileSync()..to fix the bug' try this time' quit '

--- web.js
+++ web.js
@@ -3,14 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
- fs.readFile('/etc/passwd', function (err, data) {
-  if (err) throw err;
-  var buffer = new Buffer(36);
-  buffer.write(data, "utf-8");
-  response.send(buffer.toString("utf-8", 0, 33));
-});
- 
+	var fs = require('fs'); 
+        var content= fs.readFileSync('./index.html', 'utf-8');
+	response.send(content);
 });
 
 var port = process.env.PORT || 5000;
